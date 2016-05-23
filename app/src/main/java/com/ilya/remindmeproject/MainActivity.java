8c393ea443f3a8c0999d1cc5e69b8ce1d6c6d200
olkhovyk <ilya.olk@mail.ru>
2016-05-23 12:18:23 +0300

    Create MainActivity

---
+++ app/src/main/java/com/ilya/remindmeproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ilya.remindmeproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ilya on 23.05.2016.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
