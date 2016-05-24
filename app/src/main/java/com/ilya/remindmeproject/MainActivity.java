3a800a6bd86c8fd11914cb27c52a03116d7a55b1
olkhovyk <ilya.olk@mail.ru>
2016-05-24 09:52:11 +0300

    Create MainActivity

--- app/src/main/java/com/ilya/remindmeproject/MainActivity.java
+++ app/src/main/java/com/ilya/remindmeproject/MainActivity.java
@@ -1,15 +1,35 @@
 package com.ilya.remindmeproject;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by ilya on 23.05.2016.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
